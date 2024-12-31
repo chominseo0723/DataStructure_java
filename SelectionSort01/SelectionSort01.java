77fcc14f63040604edb64d2c46af45f0c1ef1c65
Mins_ <165632548+chominseo0723@users.noreply.github.com>
2024-12-31 20:52:49 +0900

    Update SelectionSort01.java

--- SelectionSort01/SelectionSort01.java
+++ SelectionSort01/SelectionSort01.java
@@ -2,11 +2,11 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class SelectionSort {
     public void sort(int[] A) {
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 0; i < A.length; i++) { //list[i]부터 list[n-1]까지 정렬
             int min = i;
             for (int k = i + 1; k < A.length; k++) {
                 if (A[min] > A[k]) {
-                    min = k;
+                    min = k; // 더 작은 것이 있으면 최소값을 k로
                 }
             }
             int temp = A[i];
@@ -15,7 +15,7 @@ public class SelectionSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // 잘 정렬되는지 확인하는 코드
         SelectionSort check = new SelectionSort();
         int[] input = {10, 4, 5, 2, 1, 8, 3, 6};
         check.sort(input);
@@ -23,4 +23,4 @@ public class SelectionSort {
             System.out.print(input[i] + " ");
     }
 }
-// Ctrl + Alt + L 예브게 줄맞춤
+// Ctrl + Alt + L 예쁘게 줄맞춤
